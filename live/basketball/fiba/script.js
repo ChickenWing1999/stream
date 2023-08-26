20f6d3ad259368df5e9d2ff4cc7c538f0a9e138b
ChickenWing1999 <87225861+ChickenWing1999@users.noreply.github.com>
2023-08-26 14:05:12 +0800

    Update script.js

--- live/basketball/fiba/script.js
+++ live/basketball/fiba/script.js
@@ -8,8 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
         if (liveStreamId) {
             streamContainer.style.display = "block"; // Show the live streaming container
-            streamContainer.innerHTML = `<iframe src="https://1stream.eu/game/${liveStreamId}" width="100%" height="420" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen="yes"  allow="encrypted-media"></iframe>`;
-            streamContainer.innerHTML = `<iframe src="//streamingnow.pro/tvon.php?hd=${liveStreamId}" width="100%" height="420" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen="yes"  allow="encrypted-media"></iframe>`;
+            streamContainer.innerHTML = `<iframe src="https://${liveStreamId}" width="100%" height="420" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen="yes"  allow="encrypted-media"></iframe>`;
             scrollTo(streamContainer); // Scroll to the live streaming container
         } else {
             streamContainer.style.display = "none"; // Hide the live streaming container if no valid stream ID
@@ -24,15 +23,15 @@ document.addEventListener("DOMContentLoaded", () => {
         // Replace this with your actual logic to fetch the live stream ID for the selected game
         // You might use an object or a database to store the mapping of game IDs to live stream IDs
         const gameStreamMap = {
-            game1: "puerto-rico-south-sudan-live-stream/681951?embed=1",
-            game2: "georgia-cape-verde-live-stream/681955?embed=1",
-            game3: "greece-jordan-live-stream/681953?embed=1",
-            game4: "brazil-iran-live-stream/681957?embed=1",
-            game5: "venezuela-slovenia-live-stream/681956?embed=1",
-            game6: "serbia-china-live-stream/681952?embed=1",
-            game7: "usa-new-zealand-live-stream/681954?embed=1",
+            game1: "1stream.eu/game/puerto-rico-south-sudan-live-stream/681951?embed=1",
+            game2: "1stream.eu/game/georgia-cape-verde-live-stream/681955?embed=1",
+            game3: "1stream.eu/game/greece-jordan-live-stream/681953?embed=1",
+            game4: "1stream.eu/game/brazil-iran-live-stream/681957?embed=1",
+            game5: "1stream.eu/game/venezuela-slovenia-live-stream/681956?embed=1",
+            game6: "1stream.eu/game/serbia-china-live-stream/681952?embed=1",
+            game7: "1stream.eu/game/usa-new-zealand-live-stream/681954?embed=1",
             game8: "",
-            game9: "103",
+            game9: "https://streamingnow.pro/tvon.php?hd=103",
             // Add more mappings here
         };
 
