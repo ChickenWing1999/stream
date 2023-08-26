37bf96e147397b0999acf136494b91e4b2374f08
ChickenWing1999 <87225861+ChickenWing1999@users.noreply.github.com>
2023-08-26 12:44:01 +0800

    Update script.js

--- live/basketball/fiba/script.js
+++ live/basketball/fiba/script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
         if (liveStreamId) {
             streamContainer.style.display = "block"; // Show the live streaming container
-            streamContainer.innerHTML = `<iframe src="https://streamingnow.pro/tvon.php?hd=${liveStreamId}" width="100%" height="500px" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen  allow="encrypted-media"></iframe>`;
+            streamContainer.innerHTML = `<iframe src="https://1stream.eu/game/${liveStreamId}" width="100%" height="500px" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen  allow="encrypted-media"></iframe>`;
             scrollTo(streamContainer); // Scroll to the live streaming container
         } else {
             streamContainer.style.display = "none"; // Hide the live streaming container if no valid stream ID
@@ -22,9 +22,14 @@ document.addEventListener("DOMContentLoaded", () => {
         // Replace this with your actual logic to fetch the live stream ID for the selected game
         // You might use an object or a database to store the mapping of game IDs to live stream IDs
         const gameStreamMap = {
-            game1: "86",
-            game2: "",
-            game3: "stream_id_3",
+            game1: "puerto-rico-south-sudan-live-stream/681951?embed=1",
+            game2: "georgia-cape-verde-live-stream/681955?embed=1",
+            game3: "greece-jordan-live-stream/681953?embed=1",
+            game4: "brazil-iran-live-stream/681957?embed=1",
+            game5: "venezuela-slovenia-live-stream/681956?embed=1",
+            game6: "serbia-china-live-stream/681952?embed=1",
+            game7: "usa-new-zealand-live-stream/681954?embed=1",
+            game8: "?embed=1",
             // Add more mappings here
         };
 
