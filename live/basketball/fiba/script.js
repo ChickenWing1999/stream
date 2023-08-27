6e43af61f5ab2b0aebd677f35662ef6ade1c4267
ChickenWing1999 <87225861+ChickenWing1999@users.noreply.github.com>
2023-08-27 09:39:48 +0800

    Update script.js

--- live/basketball/fiba/script.js
+++ live/basketball/fiba/script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
     
         if (liveStreamId) {
             streamContainer.style.display = "block"; // Show the live streaming container
-            streamContainer.innerHTML = `<iframe src="https://${liveStreamId}" width="100%" height="420" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen="yes"  allow="encrypted-media"></iframe>`;
+            streamContainer.innerHTML = `<iframe src="${liveStreamId}" width="100%" height="420" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen="yes"  allow="encrypted-media"></iframe>`;
             scrollTo(streamContainer); // Scroll to the live streaming container
         } else {
             streamContainer.style.display = "none"; // Hide the live streaming container if no valid stream ID
@@ -23,15 +23,15 @@ document.addEventListener("DOMContentLoaded", () => {
         // Replace this with your actual logic to fetch the live stream ID for the selected game
         // You might use an object or a database to store the mapping of game IDs to live stream IDs
         const gameStreamMap = {
-            game1: "1stream.eu/game/puerto-rico-south-sudan-live-stream/681951?embed=1",
-            game2: "1stream.eu/game/georgia-cape-verde-live-stream/681955?embed=1",
-            game3: "1stream.eu/game/greece-jordan-live-stream/681953?embed=1",
-            game4: "1stream.eu/game/brazil-iran-live-stream/681957?embed=1",
-            game5: "1stream.eu/game/venezuela-slovenia-live-stream/681956?embed=1",
-            game6: "1stream.eu/game/serbia-china-live-stream/681952?embed=1",
-            game7: "1stream.eu/game/usa-new-zealand-live-stream/681954?embed=1",
-            game8: "",
-            game9: "streamingnow.pro/tvon.php?hd=103",
+            game1: "https://streamingnow.pro/tvon.php?hd=86",
+            game2: "https://streamingnow.pro/tvon.php?hd=88",
+            game3: "https://streamingnow.pro/tvon.php?hd=85",
+            game4: "https://streamingnow.pro/tvon.php?hd=87",
+            game5: "https://streamingnow.pro/tvon.php?hd=88",
+            game6: "https://streamingnow.pro/tvon.php?hd=86",
+            game7: "https://streamingnow.pro/tvon.php?hd=85",
+            game8: "https://streamingnow.pro/tvon.php?hd=87",
+            game9: "https://streamingnow.pro/tvon.php?hd=103",
             // Add more mappings here
         };
 
