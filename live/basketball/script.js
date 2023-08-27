de0a14b2b8ec2c7d7d99627ecde63a86c981f643
ChickenWing1999 <87225861+ChickenWing1999@users.noreply.github.com>
2023-08-27 10:00:21 +0800

    Update script.js

--- live/basketball/script.js
+++ live/basketball/script.js
@@ -5,10 +5,16 @@ $(document).ready(function() {
     watchButton.on("click", function() {
         const selectedGame = $("#gameSelect").val();
         const liveStreamUrl = getLiveStreamUrl(selectedGame);
+        const gameStartTime = getGameStartTime(selectedGame);
 
-        streamContainer.css("display", "block");
-        streamContainer.html(`<iframe src="${liveStreamUrl}" width="100%" height="400" frameborder="0"></iframe>`);
-        scrollTo(streamContainer);
+        showCountdownTimer(gameStartTime);
+
+        setTimeout(function() {
+            streamContainer.css("display", "block");
+            streamContainer.html(`<iframe src="${liveStreamUrl}" width="100%" height="400" frameborder="0"></iframe>`);
+            scrollTo(streamContainer);
+            showLiveIndicator();
+        }, gameStartTime * 1000);
     });
 
     // Replace with your logic to map games to streaming URLs
@@ -26,5 +32,30 @@ $(document).ready(function() {
         $("html, body").animate({
             scrollTop: element.offset().top - 20
         }, 1000);
+    }
+});
+
+ function showCountdownTimer(startTime) {
+        const countdownElement = $("#countdown");
+        countdownElement.text("Game starts in");
+        const interval = setInterval(function() {
+            const now = Math.floor(Date.now() / 1000);
+            const remainingTime = startTime - now;
+            if (remainingTime > 0) {
+                const minutes = Math.floor(remainingTime / 60);
+                const seconds = remainingTime % 60;
+                countdownElement.text(`Game starts in ${minutes}:${seconds}`);
+            } else {
+                clearInterval(interval);
+                countdownElement.text("Game is live!");
+            }
+        }, 1000);
+    }
+
+    function showLiveIndicator() {
+        const liveIndicator = $("<div>")
+            .addClass("live-indicator")
+            .text("LIVE")
+            .appendTo($("#streamContainer"));
     }
 });
