50cbdbb400005eac062878ac888a503409515078
ChickenWing1999 <87225861+ChickenWing1999@users.noreply.github.com>
2023-09-10 19:22:15 +0800

    Update script.js

--- live/channel/nfl-network/script.js
+++ live/channel/nfl-network/script.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
     
         if (liveStreamId) {
             streamContainer.style.display = "block"; // Show the live streaming container
-            streamContainer.innerHTML = `<iframe src="${liveStreamId}" width="100%" height="500px" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen  allow="encrypted-media"></iframe>`;
+            streamContainer.innerHTML = `<iframe src="${liveStreamId}" width="100%" height="420px" marginheight="0" marginwidth="0" scrolling="no" frameborder="0" allowfullscreen  allow="encrypted-media"></iframe>`;
             scrollTo(streamContainer); // Scroll to the live streaming container
         } else {
             streamContainer.style.display = "none"; // Hide the live streaming container if no valid stream ID
