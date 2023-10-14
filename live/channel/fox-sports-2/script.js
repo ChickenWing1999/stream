7b8ed53bb4672849f17599c62eb3688ddd5e5a32
ChickenWing1999 <87225861+ChickenWing1999@users.noreply.github.com>
2023-10-14 22:21:40 +0800

    Update script.js

--- live/channel/fox-sports-2/script.js
+++ live/channel/fox-sports-2/script.js
@@ -34,7 +34,7 @@ document.addEventListener("DOMContentLoaded", () => {
         // Replace this with your actual logic to fetch the live stream ID for the selected game
         // You might use an object or a database to store the mapping of game IDs to live stream IDs
         const gameStreamMap = {
-            game9: "https://streamingnow.pro/tvon.php?hd=188",
+            game9: "https://freeviplive.com/tvon.php?hd=188",
             // Add more mappings here
         };
 
