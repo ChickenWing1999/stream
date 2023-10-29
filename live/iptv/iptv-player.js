a2aa11863463ded47f191c93bdc1b8081a95ee22
ChickenWing1999 <87225861+ChickenWing1999@users.noreply.github.com>
2023-10-29 18:01:02 +0800

    Update iptv-player.js

--- live/iptv/iptv-player.js
+++ live/iptv/iptv-player.js
@@ -1,70 +1,36 @@
 document.addEventListener("DOMContentLoaded", function () {
     const player = videojs("iptv-player");
     const channelList = document.getElementById("channel-list");
-    const channelThumbnail = document.getElementById("channel-thumbnail-img");
 
-    // Define a list of IPTV playlists
-    const iptvPlaylists = [
-        {
-            name: "Main",
-            url: "https://iptv-org.github.io/iptv/index.m3u",
-        },
-        {
-            name: "NSFW",
-            url: "https://iptv-org.github.io/iptv/index.nsfw.m3u",
-        },
-        // Add more playlists here
-    ];
+    // Define your M3U playlist
+    const iptvPlaylistURL = "https://iptv-org.github.io/iptv/index.m3u";
 
-    // Fetch and parse the selected playlist
-    function loadPlaylist(selectedPlaylistURL) {
-        fetch(selectedPlaylistURL)
-            .then((response) => response.text())
-            .then((data) => {
-                const channels = parseM3UPlaylist(data);
+    // Fetch and parse the M3U playlist
+    fetch(iptvPlaylistURL)
+        .then((response) => response.text())
+        .then((data) => {
+            const channels = parseM3UPlaylist(data);
 
-                // Populate the channel list
-                channelList.innerHTML = '<option value="">Select a channel</option>';
-                channels.forEach((channel) => {
-                    const option = document.createElement("option");
-                    option.value = channel.url;
-                    option.text = channel.name;
-                    channelList.appendChild(option);
-                });
-
-                // Handle channel selection
-                channelList.addEventListener("change", function () {
-                    const selectedChannelURL = channelList.value;
-                    if (selectedChannelURL) {
-                        player.src(selectedChannelURL);
-                        player.play();
-                        // Set the channel thumbnail if available
-                        channelThumbnail.src = channels.find((channel) => channel.url === selectedChannelURL)?.thumbnail || "";
-                    }
-                });
-            })
-            .catch((error) => {
-                console.error("Failed to load the IPTV playlist:", error);
+            // Populate the channel list
+            channels.forEach((channel) => {
+                const option = document.createElement("option");
+                option.value = channel.url;
+                option.text = channel.name;
+                channelList.appendChild(option);
             });
-    }
 
-    // Populate the playlist selector
-    const playlistSelector = document.getElementById("playlist-selector");
-    iptvPlaylists.forEach((playlist, index) => {
-        const option = document.createElement("option");
-        option.value = index;
-        option.text = playlist.name;
-        playlistSelector.appendChild(option);
-    });
-
-    // Load the selected playlist
-    playlistSelector.addEventListener("change", function () {
-        const selectedPlaylistIndex = playlistSelector.value;
-        if (selectedPlaylistIndex !== "") {
-            const selectedPlaylistURL = iptvPlaylists[selectedPlaylistIndex].url;
-            loadPlaylist(selectedPlaylistURL);
-        }
-    });
+            // Handle channel selection
+            channelList.addEventListener("change", function () {
+                const selectedChannelURL = channelList.value;
+                if (selectedChannelURL) {
+                    player.src(selectedChannelURL);
+                    player.play();
+                }
+            });
+        })
+        .catch((error) => {
+            console.error("Failed to load the IPTV playlist:", error);
+        });
 });
 
 // Function to parse M3U playlist
@@ -77,10 +43,9 @@ function parseM3UPlaylist(data) {
         if (line.startsWith("#EXTINF:")) {
             const nameMatch = line.match(/tvg-name="([^"]+)"/);
             const url = lines[lines.indexOf(line) + 1];
-            const thumbnailMatch = line.match(/tvg-logo="([^"]+)"/);
             if (nameMatch) {
                 const name = nameMatch[1];
-                currentChannel = { name, url, thumbnail: thumbnailMatch ? thumbnailMatch[1] : "" };
+                currentChannel = { name, url };
                 channels.push(currentChannel);
             }
         }
