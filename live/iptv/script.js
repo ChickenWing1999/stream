dc33d4bb7495283f42cb4ea8461bf706ec920431
ChickenWing1999 <87225861+ChickenWing1999@users.noreply.github.com>
2023-10-29 18:11:43 +0800

    Update script.js

--- live/iptv/script.js
+++ live/iptv/script.js
@@ -1,30 +1,42 @@
-// Your M3U playlist URLs
 const playlists = [
-    'https://iptv-org.github.io/iptv/index.m3u',
-    'https://iptv-org.github.io/iptv/index.nsfw.m3u',
-    // Add more playlists as needed
+    {
+        category: 'News',
+        channels: [
+            { name: 'News', url: 'https://iptv-org.github.io/iptv/categories/news.m3u' },
+        ],
+    },
+    {
+        category: 'Sports',
+        channels: [
+            { name: 'Sports', url: 'https://iptv-org.github.io/iptv/categories/sports.m3u' },
+        },
+    },
+    // Add more categories and channels as needed
 ];
 
+const categoryList = document.getElementById('category-list');
 const channelList = document.getElementById('channel-list');
 const channelPlayer = document.getElementById('channel-player');
 
-// Fetch and populate channel list
-playlists.forEach(playlistUrl => {
-    fetch(playlistUrl)
-        .then(response => response.text())
-        .then(data => {
-            const parser = new DOMParser();
-            const xmlDoc = parser.parseFromString(data, 'text/xml');
-            const channelNodes = xmlDoc.querySelectorAll('channel');
+// Populate channel categories
+playlists.forEach(category => {
+    const li = document.createElement('li');
+    li.innerHTML = `<a href="#" data-category="${category.category}">${category.category}</a>`;
+    categoryList.appendChild(li);
+});
 
-            channelNodes.forEach(channelNode => {
-                const name = channelNode.querySelector('display-name').textContent;
-                const url = channelNode.querySelector('url').textContent;
-                const li = document.createElement('li');
-                li.innerHTML = `<a href="${url}" data-url="${url}">${name}</a>`;
-                channelList.appendChild(li);
-            });
+// Display channels for the selected category
+categoryList.addEventListener('click', (e) => {
+    if (e.target.tagName === 'A') {
+        e.preventDefault();
+        const selectedCategory = e.target.getAttribute('data-category');
+        channelList.innerHTML = '';
+        playlists.find(category => category.category === selectedCategory).channels.forEach(channel => {
+            const li = document.createElement('li');
+            li.innerHTML = `<a href="${channel.url}" data-url="${channel.url}">${channel.name}</a>`;
+            channelList.appendChild(li);
         });
+    }
 });
 
 // Play selected channel when a link is clicked
