1ca13a5ad157bf4d5ab494e3df0433bf9e8392ee
ChickenWing1999 <87225861+ChickenWing1999@users.noreply.github.com>
2024-05-16 17:05:57 +0800

    Update script.js

--- live/channel/espn-2/script.js
+++ live/channel/espn-2/script.js
@@ -47,4 +47,24 @@ document.addEventListener("DOMContentLoaded", () => {
             top: element.offsetTop - 20
         });
     }
+
+if(Hls.isSupported()) {
+    var video = document.getElementById('video');
+    var hls = new Hls();
+    hls.loadSource('http://cdncities.com/deranalive/deranalive/playlist.m3u8');
+    hls.attachMedia(video);
+    hls.on(Hls.Events.MANIFEST_PARSED,function() {
+      video.play();
+  });
+ }
+ // hls.js is not supported on platforms that do not have Media Source Extensions (MSE) enabled.
+ // When the browser has built-in HLS support (check using `canPlayType`), we can provide an HLS manifest (i.e. .m3u8 URL) directly to the video element throught the `src` property.
+ // This is using the built-in support of the plain video element, without using hls.js.
+  else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+    video.src = 'https://cdn1.skygo.mn/live/disk1/Cartoon_Network/HLS-FTA/Cartoon_Network.m3u8';
+    video.addEventListener('canplay',function() {
+      video.play();
+    });
+  }
+    
 });
