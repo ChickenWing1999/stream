092adc04d14fef5202f22483fd26b5d1da7e34ee
ChickenWing1999 <87225861+ChickenWing1999@users.noreply.github.com>
2024-07-23 14:13:30 +0800

    Update script.js

--- live/channels/adult-channel/script.js
+++ live/channels/adult-channel/script.js
@@ -1,19 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
     const gallery = document.querySelector('.gallery');
 
-    const videoLinks = [
-        'https://www.youtube.com/embed/dQw4w9WgXcQ',
-        'https://www.youtube.com/embed/tgbNymZ7vqY',
-        // Add more video links here...
+    const videoData = [
+        {
+            link: 'https://www.youtube.com/embed/dQw4w9WgXcQ',
+            thumbnail: 'https://img.youtube.com/vi/dQw4w9WgXcQ/0.jpg',
+            title: 'Video 1'
+        },
+        {
+            link: 'https://www.youtube.com/embed/tgbNymZ7vqY',
+            thumbnail: 'https://img.youtube.com/vi/tgbNymZ7vqY/0.jpg',
+            title: 'Video 2'
+        }
+        // Add more video objects here...
     ];
 
-    for (let i = 0; i < 100; i++) {
-        const videoLink = videoLinks[i % videoLinks.length]; // Cycle through the video links
+    videoData.forEach(video => {
         const videoItem = document.createElement('div');
         videoItem.className = 'video-item';
         videoItem.innerHTML = `
-            <iframe src="${videoLink}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <div class="thumbnail" style="background-image: url('${video.thumbnail}');"></div>
+            <iframe src="${video.link}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <div class="overlay">${video.title}</div>
         `;
         gallery.appendChild(videoItem);
-    }
+    });
 });
