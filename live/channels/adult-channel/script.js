03ed7bdea8d54e463e99b8d85cfc6fd951c67d40
ChickenWing1999 <87225861+ChickenWing1999@users.noreply.github.com>
2024-07-23 14:17:12 +0800

    Update script.js

--- live/channels/adult-channel/script.js
+++ live/channels/adult-channel/script.js
@@ -4,12 +4,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const videoData = [
         {
             link: 'https://www.youtube.com/embed/dQw4w9WgXcQ',
-            thumbnail: 'https://img.youtube.com/vi/dQw4w9WgXcQ/0.jpg',
             title: 'Video 1'
         },
         {
             link: 'https://www.youtube.com/embed/tgbNymZ7vqY',
-            thumbnail: 'https://img.youtube.com/vi/tgbNymZ7vqY/0.jpg',
             title: 'Video 2'
         }
         // Add more video objects here...
@@ -19,7 +17,6 @@ document.addEventListener('DOMContentLoaded', () => {
         const videoItem = document.createElement('div');
         videoItem.className = 'video-item';
         videoItem.innerHTML = `
-            <div class="thumbnail" style="background-image: url('${video.thumbnail}');"></div>
             <iframe src="${video.link}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             <div class="overlay">${video.title}</div>
         `;
