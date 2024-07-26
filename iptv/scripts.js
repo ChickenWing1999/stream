c4562214871b2beb418053f9cd457cc200fbe528
ChickenWing1999 <87225861+ChickenWing1999@users.noreply.github.com>
2024-07-27 00:33:42 +0800

    Update scripts.js

--- iptv/scripts.js
+++ iptv/scripts.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
     const channels = [
-        { name: 'Cartoonito', link: 'https://player.livepush.io/emv5POxKMYWPB', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Cartoonito_-_Logo_2021.svg/1000px-Cartoonito_-_Logo_2021.svg.png' },
-        { name: 'TV5 HD', link: 'https://player.livepush.io/emvjUNWUzuU2J', image: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/e6/TV5_HD_Logo_2023.svg/2560px-TV5_HD_Logo_2023.svg.png' },
-        { name: 'ANC HD', link: 'https://player.livepush.io/emvptOp3hIomK', image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/ABS-CBN_News_Channel_%282015%29.svg/1200px-ABS-CBN_News_Channel_%282015%29.svg.png' },
+        { name: 'Cartoonito', link: 'https://player.livepush.io/emv5POxKMYWPB' },
+        { name: 'TV5 HD', link: 'https://player.livepush.io/emvjUNWUzuU2J' },
+        { name: 'ANC HD', link: 'https://player.livepush.io/emvptOp3hIomK' },
         // Add more channels as needed
     ];
 
@@ -14,20 +14,10 @@ document.addEventListener('DOMContentLoaded', function () {
 
     channels.forEach((channel, index) => {
         const li = document.createElement('li');
-        const img = document.createElement('img');
-        const span = document.createElement('span');
-
-        img.src = channel.image;
-        img.classList.add('channel-image');
-        span.textContent = `${index + 1}. ${channel.name}`;
-
-        li.appendChild(img);
-        li.appendChild(span);
-
+        li.textContent = `${index + 1}. ${channel.name}`;
         li.addEventListener('click', () => {
             liveStream.src = channel.link;
         });
-
         channelUl.appendChild(li);
     });
 
@@ -45,8 +35,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const filter = searchBox.value.toLowerCase();
         const li = channelUl.getElementsByTagName('li');
         for (let i = 0; i < li.length; i++) {
-            const span = li[i].getElementsByTagName('span')[0];
-            const txtValue = span.textContent || span.innerText;
+            const txtValue = li[i].textContent || li[i].innerText;
             li[i].style.display = txtValue.toLowerCase().indexOf(filter) > -1 ? '' : 'none';
         }
     });
