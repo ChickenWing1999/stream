46309b27fa7477a5be4231674aeee39d2403d2cb
ChickenWing1999 <87225861+ChickenWing1999@users.noreply.github.com>
2024-08-15 06:24:29 +0800

    Update scripts.js

--- test/scripts.js
+++ test/scripts.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
     const channels = [
         { name: 'ABS-CBN', link: 'https://dlhd.sx/embed/stream-785.php', type: 'embed' },
+        { name: 'ONE Sports', link: 'https://bit.ly/4dIklUF', type: 'hls' },
+        { name: 'ONE Sports+', link: 'https://tinyurl.com/SPORTplu', type: 'hls' },
         { name: 'HBO', link: 'aHR0cHM6Ly9xcC1wbGR0LWxpdmUtZ3JwLTAzLXByb2QuYWthbWFpemVkLm5ldC9vdXQvdS9oYm9oZF9xcC5tM3U4', type: 'hls' },
         { name: 'CINE MO!', link: 'https://cinemo-abscbn-ono.amagi.tv/playlist.m3u8', type: 'hls' },
-        { name: 'Cinema One', link: 'aHR0cHM6Ly9jaW5lbWFvbmUtYWJzY2JuLW9uby5hbWFnaS50di9pbmRleC5tM3U4', type: 'hls' },
+        { name: 'Cinema One', link: 'https://cinemaone-abscbn-ono.amagi.tv/index.m3u8', type: 'hls' },
         { name: 'MYX', link: 'https://myxnola-abscbn-ono.amagi.tv/index.m3u8', type: 'hls' },
         { name: 'ANC', link: 'https://ancglobal-abscbn-ono.amagi.tv/playlist.m3u8', type: 'hls' },
         { name: 'True FM', link: 'https://qp-pldt-live-grp-05-prod.akamaized.net/out/u/radyo5_qp.m3u8', type: 'hls' },
